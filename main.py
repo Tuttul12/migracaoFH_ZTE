591c9165cafe82eb22dd6aade56f8e2a959b9340
Tuttul12 <luiiz.tuttu@icloud.com>
2023-01-11 19:43:36 -0300

    slrjcnbsakjlfbvnakjsf

--- main.py
+++ main.py
@@ -1,54 +1,90 @@
-import paramiko
-import os
+import getpass
+import telnetlib
 import pandas as pd
 import time
 
 df = pd.read_csv('teste.csv')
-address = '10.7.0.66'
-login = "noc"
-passwd= "6O7noNaoiMClIn7e"
-con = paramiko.SSHClient()
-con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+HOST = '10.7.0.66'
+user = input("Digite o login: ")
+password= getpass.getpass()
 
-con.connect(address, port = 22, username = login, password = passwd , timeout= 3)
-for i,linhas in df.iterrows():
-    if 'AN5506' in df.loc[i,'MODELO']:
-        try:
-            stdin, stdout, stderr = con.exec_command("conf t")
-            stdin, stdout,stderr = con.exec_command("interface gpon-olt_",df.loc[i, 'PON'])
-            stdin, stdout,stderr = con.exec_command("onu "+ df.loc[i, 'NUMERO'] +" type F601 sn "+ df.loc[i, 'SERIAL'])
-            stdin, stdout,stderr = con.exec_command("exit")
-            stdin, stdout,stderr = con.exec_command("interface gpon-onu_2: " + df.loc[i, 'NUMERO'])
-            stdin, stdout,stderr = con.exec_command("description "+ df.loc[i, 'DESCRIPTION'])
-            stdin, stdout,stderr = con.exec_command("tcont 1 profile 1GB_UP")
-            stdin, stdout,stderr = con.exec_command("gemport 1 tcont 1")
-            stdin, stdout,stderr = con.exec_command("gemport 1 traffic-limit downstream 1GB_DW")
-            stdin, stdout,stderr = con.exec_command("service-port 1 vport 1 user-vlan  70 vlan  70")
-            stdin, stdout,stderr = con.exec_command("exit")
-            stdin, stdout,stderr = con.exec_command("pon-onu-mng gpon-onu_"+ df.loc[i,'PON'] +":"+ df.loc[i, 'NUMERO'])
-            stdin, stdout,stderr = con.exec_command("service 1 gemport 1 vlan 70;")
-            stdin, stdout,stderr = con.exec_command("vlan port eth_0/1 mode tag vlan 70")
-            print(stdout.read().decode('ascii'))
-            con.close()
-        except:
-            print("Falha no:", df.loc[i,'DESCRIPTION'], df.loc[i, 'MODELO'])
-    else:
-        try:
-            stdin, stdout, stderr = con.exec_command("conf t")
-            stdin, stdout, stderr = con.exec_command("interface gpon-olt_",df.loc[i, 'PON'])
-            stdin, stdout,stderr = con.exec_command("onu"+ df.loc[i, 'NUMERO'] +"type F601 sn "+ df.loc[i, 'SERIAL'])
-            stdin, stdout,stderr = con.exec_command("exit")
-            stdin, stdout,stderr = con.exec_command("interface gpon-onu_"+ df.loc[i, 'SERIAL'])
-            stdin, stdout,stderr = con.exec_command("description " + df.loc[i, 'DESCRIPTION'])
-            stdin, stdout,stderr = con.exec_command("tcont 1 profile 1GB_UP")
-            stdin, stdout,stderr = con.exec_command("gemport 1 tcont 1")
-            stdin, stdout,stderr = con.exec_command("gemport 1 traffic-limit downstream 1GB_DW")
-            stdin, stdout,stderr = con.exec_command("service-port 1 vport 1 user-vlan 70 vlan 70")
-            stdin, stdout,stderr = con.exec_command("exit")
-            stdin, stdout,stderr = con.exec_command("pon-onu-mng gpon-onu_"+ df.loc[i,'PON']+":"+df.loc[i, 'NUMERO'])
-            stdin, stdout,stderr = con.exec_command("service 1 gemport 1 vlan 70;")
-            stdin, stdout,stderr = con.exec_command("vlan port eth_0/1 mode tag vlan 70")
-            stdin, stdout,stderr = con.exec_command("security-mgmt 212 state enable mode forward protocol web")
-        except:
-            print("Falha no:", df.loc[i,'DESCRIPTION'], df.loc[i, 'MODELO'])
-            con.close()
+tn= telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username:")
+tn.write(user.encode('ascii') + b"\n")
+time.sleep(1)
+if password:
+    tn.read_until(b"Password:")
+    tn.write(password.encode('ascii') + b"\n")
+
+for i,linhas in df .iterrows():
+    sn= df.loc[i, 'SERIAL']
+    num= df.loc[i, 'NUMERO']
+    pon= df.loc [i, 'PON']
+    desc= df.loc[i, 'DESCRIPTION']
+    model= df.loc[i, 'MODELO']
+
+    sn_ = bytes(sn, 'ascii')
+    num1 = str(num)
+    num_ = bytes(num1, 'ascii')
+    pon1 = str(pon)
+    pon_ = bytes(pon1, 'ascii')
+    desc_ = bytes(desc, 'ascii')
+    if 'AN5506' in model:
+        time.sleep(1)
+        tn.write(b"configure terminal\n")
+        print("conf t")
+        time.sleep(1)
+        tn.write(b"interface gpon-olt_1/2/"+pon_)
+        print("interface gpon-olt_1/2/",pon)
+        time.sleep(1)
+        tn.write(b"\n")
+        time.sleep(1)
+        tn.write(b"onu "+num_)
+        time.sleep(1)
+        tn.write(b" type F601 sn "+sn_)
+        time.sleep(1)
+        tn.write(b"\n")
+        print("onu type F601 sn",sn)
+        time.sleep(1)
+        tn.write(b"exit\n")
+        print("exit")
+        time.sleep(1)
+        tn.write(b"interface gpon-onu_1/2/"+pon_)
+        time.sleep(1)
+        tn.write(b":"+num_)
+        time.sleep(1)
+        tn.write(b"\n")
+        print("interface gpon-onu_",pon)
+        time.sleep(1)
+        tn.write(b"description "+desc_)
+        tn.write(b"\n")
+        print("description",desc)
+        time.sleep(1)
+        tn.write(b"tcont 1 profile 1GB_UP\n")
+        print("tcont 1 profile 1GB_UP")
+        time.sleep(1)
+        tn.write(b"gemport 1 tcont 1\n")
+        print("gemport 1 tcont 1")
+        time.sleep(1)
+        tn.write(b"gemport 1 traffic-limit downstream 1GB_DW\n")
+        print("gemport 1 traffic-limit downstream 1GB_DW")
+        time.sleep(1)
+        tn.write(b"service-port 1 vport 1 user-vlan 70 vlan 70\n")
+        print("service-port 1 vport 1 user-vlan 70 vlan 70")
+        time.sleep(1)
+        tn.write(b"exit\n")
+        print("exit")
+        time.sleep(1)
+        tn.write(b"pon-onu-mng gpon-onu_1/2/"+pon_)
+        time.sleep(1)
+        tn.write(b":"+num_)
+        time.sleep(1)
+        tn.write(b"\n")
+        print("pon-onu-mng gpon-onu",pon)
+        tn.write(b"service 1 gemport 1 vlan 70\n")
+        print("service 1 gemport 1 vlan 70")
+        time.sleep(1)
+        tn.write(b"exit\n")
+        print("exit")
+        print("ONU Autorizada!")
